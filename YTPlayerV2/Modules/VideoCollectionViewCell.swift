66da2e0aac0d4faf7fab821c8f1f0d71714cb609
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-02-03 17:51:44 +0200

    video cell views added

--- YTPlayerV2/Modules/VideoCollectionViewCell.swift
+++ YTPlayerV2/Modules/VideoCollectionViewCell.swift
@@ -10,6 +10,10 @@ import UIKit
 class VideoCollectionViewCell: UICollectionViewCell {
     static let reuseIdentifier = "VideoCollectionViewCell"
 
+    @IBOutlet weak var videoImageView: UIImageView!
+    @IBOutlet weak var videoNameLabel: UILabel!
+    @IBOutlet weak var listenersCounter: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
