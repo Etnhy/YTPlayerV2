996629e01e0c97f68fd07f4b988df5916ffed62a
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-02-03 20:58:31 +0200

    YTPlayer storyboard

--- YTPlayerV2/Modules/YTPLayerViewController.swift
+++ YTPlayerV2/Modules/YTPLayerViewController.swift
@@ -6,10 +6,23 @@
 //
 
 import UIKit
-
+import WebKit
 class YTPLayerViewController: UIViewController {
     static let identifier = "YTPLayerViewController"
-
+    
+    
+    @IBOutlet weak var songProgress: UIProgressView!
+    @IBOutlet weak var yotubeWebView: WKWebView!
+    @IBOutlet weak var songNameLabel: UILabel!
+    @IBOutlet weak var listenersCountsLAbel: UILabel!
+    @IBOutlet weak var backwardButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    
+    @IBOutlet weak var showHideButton: UIButton!
+    
+     var isPlayed: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         settings()
@@ -20,8 +33,19 @@ class YTPLayerViewController: UIViewController {
             make.size.equalTo(CGSize(width: self.view.frame.width,height: 700))
         }
         self.view.layer.cornerRadius = 40
-
-        
+        self.showHideButton.layer.cornerRadius = 40
+        self.playPauseButton.addTarget(self, action: #selector(playerPauseAction), for: .touchUpInside)
+    }
+    
+    @objc fileprivate func playerPauseAction() {
+        isPlayed.toggle()
+       print(isPlayed)
+       var buttonConfig = UIButton.Configuration.filled()
+       buttonConfig.image = isPlayed ? UIImage(systemName: "pause.fill")?.applyingSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 34)) : UIImage(systemName: "play.fill")?.applyingSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 34))
+       buttonConfig.baseBackgroundColor = .clear
+//       buttonConfig.baseForegroundColor = AppColors.PlayerColor.playerButtonsColor
+       playPauseButton.configuration = buttonConfig
+    }
     }
 
-}
+
