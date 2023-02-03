85525bd2636b14565afd31900c7ea51dc4afcc79
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-02-03 17:47:33 +0200

    musicCell views added

--- YTPlayerV2/Modules/MusicCollectionViewCell.swift
+++ YTPlayerV2/Modules/MusicCollectionViewCell.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class MusicCollectionViewCell: UICollectionViewCell {
-    
     static let reuseIdentifier = "MusicCollectionViewCell"
-
+    
+    @IBOutlet weak var musicImageView: UIImageView!
+    @IBOutlet weak var musicSongName: UILabel!
+    @IBOutlet weak var listenersCountLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = .yellow
