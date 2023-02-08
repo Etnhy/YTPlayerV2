6fa3238db8a1534a2d0de85c90ccb9fc62c7cd2c
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-02-08 19:17:37 +0200

    carousel configure and fix

--- YTPlayerV2/Modules/CarouselCollectionViewCell.swift
+++ YTPlayerV2/Modules/CarouselCollectionViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 import SkeletonView
 
 class CarouselCollectionViewCell: UICollectionViewCell {
@@ -16,16 +17,33 @@ class CarouselCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var authorNameLabel: UILabel!
     @IBOutlet weak var subscribersCountLabel: UILabel!
 
+    
+    let strokeTextAttributes: [NSAttributedString.Key : Any] = [
+        .strokeColor : UIColor.black,
+        .foregroundColor : UIColor.white,
+        .strokeWidth : -3.0,
+        ]
+
     override func awakeFromNib() {
         super.awakeFromNib()
         settings()
         self.authorImageView.isSkeletonable = true
     }
+    
     fileprivate func settings () {
         self.authorImageView.backgroundColor = AppColors.backViewColor
         authorImageView.layer.cornerRadius = 8
         self.layer.cornerRadius = 8
 
+    }
+    
+    public func configureCarouselCell(model: ChannelsItems) {
+        authorNameLabel.attributedText = NSAttributedString(string: model.snippet.title, attributes: strokeTextAttributes)
+        subscribersCountLabel.text = "Subscribers: \(model.statistics.subscriberCount)"
+        let url = URL(string: "\(model.snippet.thumbnails.high.url)")
+        guard let url = url else { return }
+        authorImageView.af.setImage(withURL: url)
+        
     }
 
 }
