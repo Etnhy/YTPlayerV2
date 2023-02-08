b1ddfb9d9eb54528ca152aa477af1be68280d51f
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-02-08 19:18:41 +0200

    add high: HighTopChannelThumbnails ( качество картинки)

--- YTPlayerV2/Models/TopChannelsModel.swift
+++ YTPlayerV2/Models/TopChannelsModel.swift
@@ -36,10 +36,18 @@ struct ChannelsSnippet: Codable {
  //MARK: - ChannelThumbnails
 struct ChannelThumbnails: Codable {
     let defaultChannelThumbnails : DefaultChannelThumbnails
+    let high: HighTopChannelThumbnails
     
     enum CodingKeys: String, CodingKey {
         case defaultChannelThumbnails = "default"
+        case high
     }
+}
+ //MARK: - HighTopChannelThumbnails
+struct HighTopChannelThumbnails: Codable {
+    let url: String
+    let width: Int
+    let height: Int
 }
  //MARK: - DefaultChannelThumbnails
 struct DefaultChannelThumbnails: Codable {
@@ -47,53 +55,3 @@ struct DefaultChannelThumbnails: Codable {
     let width: Int
     let height: Int
 }
-/*
- {
-     "kind": "youtube#channelListResponse",
-     "etag": "VfmgUblnwO0aie9B2CiQ7uLzDBM",
-     "pageInfo": {
-         "totalResults": 1,
-         "resultsPerPage": 5
-     },
-     "items": [
-         {
-             "kind": "youtube#channel",
-             "etag": "d4e1sa66-5-VknpLNiRwRPcyJVk",
-             "id": "UCUM2Q6aQXYex5y36sBej0Ng",
-             "snippet": {
-                 "title": "Lebanon Hanover",
-                 "description": "",
-                 "customUrl": "@mehrboysmitrouge",
-                 "publishedAt": "2010-07-08T17:39:32Z",
-                 "thumbnails": {
-                     "default": {
-                         "url": "https://yt3.ggpht.com/ytc/AL5GRJW9Pa70BvuohAcsFKSr9yrYa_VF-MGm3YoWjIab=s88-c-k-c0x00ffffff-no-rj-mo",
-                         "width": 88,
-                         "height": 88
-                     },
-                     "medium": {
-                         "url": "https://yt3.ggpht.com/ytc/AL5GRJW9Pa70BvuohAcsFKSr9yrYa_VF-MGm3YoWjIab=s240-c-k-c0x00ffffff-no-rj-mo",
-                         "width": 240,
-                         "height": 240
-                     },
-                     "high": {
-                         "url": "https://yt3.ggpht.com/ytc/AL5GRJW9Pa70BvuohAcsFKSr9yrYa_VF-MGm3YoWjIab=s800-c-k-c0x00ffffff-no-rj-mo",
-                         "width": 800,
-                         "height": 800
-                     }
-                 },
-                 "localized": {
-                     "title": "Lebanon Hanover",
-                     "description": ""
-                 }
-             },
-             "statistics": {
-                 "viewCount": "62873623",
-                 "subscriberCount": "184000",
-                 "hiddenSubscriberCount": false,
-                 "videoCount": "17"
-             }
-         }
-     ]
- }
- */
