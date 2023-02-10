eb7f0a619e78e9f9aaea6b45c4f33b429006840e
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-02-10 17:48:13 +0200

    struct Localized

---
+++ YTPlayerV2/Models/Localized.swift
@@ -0,0 +1,13 @@
+//
+//  Localized.swift
+//  YTPlayerV2
+//
+//  Created by Евгений Маглена on 10.02.2023.
+//
+
+import Foundation
+
+struct Localized: Codable {
+    let title: String
+    let description: String
+}
