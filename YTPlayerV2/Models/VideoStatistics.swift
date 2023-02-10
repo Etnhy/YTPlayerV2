20dc1e062d8506880b365e1bbca1edae9f3c3568
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-02-10 17:47:25 +0200

    struct VideoStatistics

---
+++ YTPlayerV2/Models/VideoStatistics.swift
@@ -0,0 +1,22 @@
+//
+//  VideoStatistics.swift
+//  YTPlayerV2
+//
+//  Created by Евгений Маглена on 10.02.2023.
+//
+
+import Foundation
+
+
+struct VideoStatistics: Codable {
+    let viewCount: String
+    let likeCount: String
+}
+/*
+ "statistics": {
+     "viewCount": "8755",
+     "likeCount": "188",
+     "favoriteCount": "0",
+     "commentCount": "0"
+ }
+ */
